a08e7260207e3fcc9f885ab9de696707dd727b36
the_worst <kirkassassin@gmail.com>
2025-11-25 02:00:12 +0100

    ex03 not finished yet

---
+++ c01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main  ()
+{
+    std::string a = "HI THIS IS BRAIN";
+
+    std::string *stringPTR =  &a;
+    std::string& stringREF = a;
+
+
+    std::cout << "a adress : " << &a << std::endl;
+    std::cout << "stringREF adress : " << &stringREF << std::endl;
+    std::cout << "stringPRT adress : " << stringPTR << std::endl;
+    std::cout << "a value : " << a << std::endl;
+    std::cout << "stringPTR value : " << *stringPTR << std::endl;
+    std::cout << "stringREF value : " << stringREF << std::endl;
+}
