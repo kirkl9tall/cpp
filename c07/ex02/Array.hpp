096268ce321b8e009e81f97ea8f75064400a59da
the_worst <kirkassassin@gmail.com>
2026-02-18 13:13:17 +0000

    kj

--- c07/ex02/Array.hpp
+++ c07/ex02/Array.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
 
@@ -8,51 +9,52 @@ class Array
 {
 private:
     T *arr;
-    unsigned int size; 
+    unsigned int _size; 
 
 public:
-    Array(): arr(NULL) , size(0){}//  initialize the array  with the null 
-    Array(unsigned int n):size(n)
+    Array(): arr(NULL) , _size(0){}//  initialize the array  with the null 
+    Array(unsigned int n):_size(n)
     {
-        // create an array of  n size  type T
+        // create an array of  n _size  type T
         arr = new T[n];
     }
-    Array(Array &other)
+    Array(const Array &other)
     {
-        this->arr = new T[size];
-        this->size = other.size;
-        for (unsigned int i = 0; size > i; i++)
+        this->_size = other._size;
+        this->arr = new T[_size];
+        for (unsigned int i = 0; _size > i; i++)
             this->arr[i] = other.arr[i];
-        // creating the array  with the size and then we will  make sure to copy with a for loop all the content ;
+        // creating the array  with the _size and then we will  make sure to copy with a for loop all the content ;
     }
-    Array &operator=(Array &other)
+
+    Array &operator=(const Array &other)
     {
         // the same thing  with the copy constructor but  we have to delete the previous  array  reallocate  then copy the content 
         if (this != &other)
         {
             delete[] this->arr;
 
-            this->size = other.size;
-            this->arr = new T[size];
+            this->_size = other._size;
+            this->arr = new T[_size];
 
-            for (unsigned int i = 0; size > i; i++)
+            for (unsigned int i = 0; _size > i; i++)
                 this->arr[i] = other.arr[i];
         }
         return *this;
     }
+
     T& operator[](unsigned int index){
-        if(arr[index]>= size)
+        if(index>= _size)
             throw std::out_of_range("index out of bounds !");
         return arr[index];
     }
-    const T& operator[](unsigned int index){
-        if(arr[index]>= size)
+    const T& operator[](unsigned int index)const {
+        if(index    >= _size)
             throw std::out_of_range("index out of bounds !");
         return arr[index];
     }
-    unsigned int size() const{return size;}
-    ~Array()
-    {
+    unsigned int size() const{return _size;}
+    ~Array(){
         delete[] arr;
     }
 };
