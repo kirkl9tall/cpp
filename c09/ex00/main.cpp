d8f962151373366344f91f623b421dcbaae0b641
the_worst <kirkassassin@gmail.com>
2026-03-03 01:32:46 +0000

    ex00

--- c09/ex00/main.cpp
+++ c09/ex00/main.cpp
@@ -1,6 +1,5 @@
 #include "BitcoinExchange.hpp"
 
-
 std::string trim(const std::string &str)
 {
     std::string::size_type start = 0;
@@ -14,73 +13,90 @@ std::string trim(const std::string &str)
     return str.substr(start, end - start);
 }
 
-
-int main ()
+int main (int argc,char *argv[])
 {
-    std::fstream infil;
-    std::string  buff;
-    
-    infil.open("pl.csv");
-    
-    std::getline(infil,buff);
-    if (buff != "date,exchange_rate")
+    if (argc == 2)
     {
-        std::cout << "format not valid "<<std::endl;
-        return -1;
-    }
-
-    std::map<std::string,int> csv_file;
-    std::string line;
-    std::string key; 
-    int val;
-
-    while (std::getline(infil,line))
-    {
-        std::stringstream ss(line);
-        std::getline(ss,key,',');
-        ss >> val;
-        csv_file[key] = val;
-    }
-    // std::map<std::string,int>::iterator it_b = csv_file.begin();
-    // std::map<std::string,int>::iterator it_e = csv_file.end();
-
-    // for (;it_b != it_e;++it_b)
-    //     std::cout << it_b->first << " | " << it_b->second << std::endl;
-
-    std::fstream input;
-    std::string buff_2;
-    input.open("input.txt");
-
-    std::getline (input,buff_2);
-
-    if  (buff_2 != "date | value"){
-        std::cout << "format input  invalid "<<std::endl;
-        return -1;
-    }
-    std::map<std::string,int> inp;
-
-    while (std::getline(input,line)){
-        std::stringstream ss(line);
-        std::getline(ss,key,'|');
-        if (!(ss >> val))
+        std::fstream infil;
+        std::string  buff;
+        
+        infil.open("pl.csv");
+        
+        std::getline(infil,buff);
+        if (buff != "date,exchange_rate")
         {
-            std::cerr << "Invalid value in line: " << line << std::endl;
-            continue;
+            std::cout << "format not valid "<<std::endl;
+            return -1;
         }
-        key = trim(key);
-        ss >> val;
-        inp[key] = val;
+
+        std::map<std::string,float> csv_file;
+        std::string line;
+        std::string key; 
+        float val;
+
+        while (std::getline(infil,line))
+        {
+            std::stringstream ss(line);
+            std::getline(ss,key,',');
+            ss >> val;
+            csv_file[key] = val;
+        }
+
+        std::fstream input;
+        std::string buff_2;
+        input.open(argv[1]);
+
+        std::getline (input,buff_2);
+
+        if  (buff_2 != "date | value"){
+            std::cout << "format input  invalid "<<std::endl;
+            return -1;
+        }
+        std::map<std::string,float> inp;
+        while (std::getline(input,line))
+        {
+
+            std::stringstream ss(line);
+            if (!std::getline(ss, key, '|'))
+                continue;
+            key = trim(key);
+            // checking value --(--------------- negative  more than  1000  lenght  dyal date 
+            if (!(ss >> val) || ( key.length() != 10 || key[4] != '-' || key[7] != '-'))
+            {
+                std::cerr << "Error: bad input" << line << std::endl;
+                continue;
+            }
+            else if  ( val < 0)
+            {
+                std::cerr << "Error: not positive  num " << line << std::endl;
+                continue;
+            }
+            else if  ( val > 1000)
+            {
+                std::cerr << "Error: bigger than 1000 " << line << std::endl;
+                continue;
+            }
+
+            std::map<std::string,float>::iterator it;
+            it = csv_file.lower_bound(key);
+
+            if (it != csv_file.end() && it->first == key)
+            {
+                std::cout << it->second * val << std::endl;
+            }
+            else
+            {
+                if  (it == csv_file.begin())
+                {
+                    std::cerr << "Error: no earlier date available\n";
+                    continue;
+                }
+                --it;
+                std::cout << it->second * val << std::endl;
+            }
+        }
+
     }
-
-    std::map<std::string,int>::iterator it_b = inp.begin();
-    std::map<std::string,int>::iterator it_e = inp.end();
-
-    for (;it_b != it_e;++it_b)
-    {
-        std::map<std::string,int>::iterator fin;
-        fin = csv_file.find(it_b->first);
-        std::cout << fin->first << "|" << fin->second << std::endl;
-    }
-
-    
+    else
+        std::cout << "invalid  args !" << std::endl;
 }
